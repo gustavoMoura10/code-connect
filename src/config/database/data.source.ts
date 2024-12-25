c75f7da655971453a9ead750978e20a45373f2ff
Gustavo Moura <gustavomoura.dev@gmail.com>
2024-12-25 17:17:52 -0300

    add: data source

---
+++ src/config/database/data.source.ts
@@ -0,0 +1,24 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+console.log('VARIAVEIS:',process.env);
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env['DB_HOST'],
+  port: Number(process.env['DB_PORT']),
+  username: process.env['DB_USERNAME'],
+  password: process.env['DB_PASSWORD'],
+  database: process.env['DB_DATABASE'],
+  entities: [__dirname + '/../../**/*.entity.{js,ts}'],
+  migrations: [__dirname + '/migrations/*.{js,ts}'],
+  
+  logging: true
+};
+const AppDataSource = new DataSource(dataSourceOptions);
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
+export default AppDataSource;

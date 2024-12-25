2470686b1c4075f5595443e3bba56d082107353d
Gustavo Moura <gustavomoura.dev@gmail.com>
2024-12-25 17:45:41 -0300

    fix: Config agora funcionando com o banco ;-)

--- src/config/database/data.source.ts
+++ src/config/database/data.source.ts
@@ -1,5 +1,4 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
-console.log('VARIAVEIS:',process.env);
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env['DB_HOST'],
@@ -7,7 +6,7 @@ export const dataSourceOptions: DataSourceOptions = {
   username: process.env['DB_USERNAME'],
   password: process.env['DB_PASSWORD'],
   database: process.env['DB_DATABASE'],
-  entities: [__dirname + '/../../**/*.entity.{js,ts}'],
+  entities: [__dirname + '/entities/*.{js,ts}'],
   migrations: [__dirname + '/migrations/*.{js,ts}'],
   
   logging: true
